e07b447b4dcde0a6527c4485eef83aad35a06297
Charles <131228676+CharlesNaag@users.noreply.github.com>
2024-07-21 03:02:17 +0800

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     auth: {
         type: 'OAuth2',
         user: 'charlesrussellllovido.naag@bicol-u.edu.ph', // Replace with your Gmail address
-        clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+        clientId:608224176552-up9ch4g97e7354fj38v721jnngrida94.apps.googleusercontent.com,
         clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
         refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN
     }
