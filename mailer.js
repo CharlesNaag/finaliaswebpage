1b5fc197947321debc8d2ce0d86a9d0a2c250554
Charles <131228676+CharlesNaag@users.noreply.github.com>
2024-07-21 03:04:37 +0800

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -7,9 +7,9 @@ const transporter = nodemailer.createTransport({
     auth: {
         type: 'OAuth2',
         user: 'charlesrussellllovido.naag@bicol-u.edu.ph', // Replace with your Gmail address
-        clientId:608224176552-up9ch4g97e7354fj38v721jnngrida94.apps.googleusercontent.com,
-        clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_OAUTH_REFRESH_TOKEN
+        clientId:'608224176552-up9ch4g97e7354fj38v721jnngrida94.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken: '1//04KfzFa5gEprNCgYIARAAGAQSNgF-L9Ir_bekE68Ow6e7L6WXBklkpeq9T41fpFH7EPXIyHlSl70zHLUNQRcL2QSfPeIr7m9eCQ'
     }
 });
 
